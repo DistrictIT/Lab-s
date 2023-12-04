9936bec1d7e034c1803ae8bd4fd3ad477626ed4a
DistrictIT <144263265+DistrictIT@users.noreply.github.com>
2023-12-04 17:29:23 +0300

    Delete LR3WEB directory

--- LR3WEB/logic.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'core/index.php';
-
-$web2 =  Web2_database::getInstance();
-
-$games_table = new table_information();
-$genres_table = new Genres_table();
-$items = $games_table->get_tables1($web2->getDB());
-$genres = $genres_table->get_tables2($web2->getDB());
