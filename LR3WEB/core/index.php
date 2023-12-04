a700d656f83292cf5414d6feb87d6391e37f73b0
DistrictIT <144263265+DistrictIT@users.noreply.github.com>
2023-12-04 17:24:06 +0300

    Delete LR3WEB directory

--- LR3WEB/core/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'web2_database.php';
-require_once 'table_genres.php';
-require_once 'table_information.php';
-require_once 'users_table.php';
-require_once 'user_logic.php';
-require_once 'UserAction.php';
