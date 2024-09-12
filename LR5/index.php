cfa8f9c4ed3af6cdff8494e44d5475d64af44b05
DistrictIT <144263265+DistrictIT@users.noreply.github.com>
2024-09-12 11:45:17 +0300

    Delete LR5 directory

--- LR5/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">  
-  <?php include ($_SERVER['DOCUMENT_ROOT'] . '/LR5/parts/head.php'); ?>   
-  <?php include ($_SERVER['DOCUMENT_ROOT'] . '/LR5/parts/header.php'); ?> 
-  <body>
-    <?php include ($_SERVER['DOCUMENT_ROOT'] . '/LR5/pages/main.php'); ?>
-    <?php include ($_SERVER['DOCUMENT_ROOT'] . '/LR5/parts/footer.php'); ?>
-  </body>
-</html>
--- LR5/parts/head.php
+++
@@ -1,7 +0,0 @@
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Купить памятники Памятник №010 из красного гранита по выгодной цене в Волгограде</title> 
-    <link href="/LR5/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="/LR5/css/main.css">
-</head> 
